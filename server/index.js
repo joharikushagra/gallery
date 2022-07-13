90d09dbd16a1759cb70f24b623e7a3adbd6f2fec
joharikushagra <skjkushagra1309@gmail.com>
2022-07-14 02:16:53 +0530

    setting variables

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const routes = require("./routes/routes");
+require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 mongoose
   .connect(
-    "mongodb+srv://kushagra:kushagra12345678@cluster0.w2lg8.mongodb.net/?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.w2lg8.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("Database connected....");
